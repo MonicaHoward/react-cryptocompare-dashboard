dd34b500b853916e325d6d2b0c45f3aada8bf024
Monica Howard <jay.m.howard@gmail.com>
2018-10-27 08:17:06 -0500

    Set state and added click handlers to toggle buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const AppLayout = styled.div`
   padding: 40px;
@@ -18,7 +18,10 @@ const Logo = styled.div`
 `;
 
 const Toggle = styled.div`
-
+    cursor: pointer;
+    ${props => props.active && css`
+    text-shadow: 0px 0px 60px #000;
+    `} 
 `;
 
 const Content = styled.div`
@@ -27,28 +30,33 @@ const Content = styled.div`
 
 
 class App extends Component {
-  render() {
-    return (
-        <AppLayout>
-          <MenuBar>
-            <Logo>
-                CryptoCompare
-            </Logo>
-            <div>
-            </div>
-            <Toggle>
-                Dashboard
-            </Toggle>
-            <Toggle>
-                Settings
-            </Toggle>
-          </MenuBar>
-          <Content>
-            hello
-          </Content>
-        </AppLayout>
-    );
-  }
+    state = {
+        page: 'dashboard'
+    }
+    displayingDashboard = () => this.state.page ==='dashboard';
+    displayingSettings = () => this.state.page ==='settings';
+    render() {
+        return (
+            <AppLayout>
+                <MenuBar>
+                    <Logo>
+                        CryptoCompare
+                    </Logo>
+                    <div>
+                    </div>
+                    <Toggle onClick={()=>{this.setState({page: 'dashboard'})}} active={this.displayingDashboard()}>
+                        Dashboard
+                    </Toggle>
+                    <Toggle onClick={()=>{this.setState({page: 'settings'})}} active={this.displayingSettings()}>
+                        Settings
+                    </Toggle>
+                  </MenuBar>
+                  <Content>
+                      Hello, I'm {this.state.page}
+                  </Content>
+            </AppLayout>
+        );
+    }
 }
 
 export default App;
