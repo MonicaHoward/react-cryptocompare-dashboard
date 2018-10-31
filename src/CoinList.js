e4aad14ed30b382bf029b9ab2aebfb0845eb6cef
Monica Howard <jay.m.howard@gmail.com>
2018-10-30 21:09:26 -0500

    Styling for CoinList

--- src/CoinList.js
+++ src/CoinList.js
@@ -1,27 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
+import {subtleBoxShadow} from "./Style";
 
 const CoinGrid = styled.div`
     display: grid;
     grid-template-columns: repeat(5, 1fr);
     grid-gap: 15px;
+    margin-top: 40px;
 `;
 const CoinTile = styled.div`
+    ${subtleBoxShadow}
     background: rgba(0, 0, 0, .3);
-
-    border: 1px solid rgba(255, 255, 255, 0.3);
+    border: 1px solid rgba(0, 0, 0, 0.3);
     padding: 10px;
     &:hover {
         cursor: pointer;
         color: purple;
     }
 `;
+const CoinHeader = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+   
+`
+const CoinSymbol = styled.div`
+    justify-self: right;
+`
 
 export default function () {
     return(
         <CoinGrid>
-            {Object.keys(this.state.coinList).map(coin =>
-                <CoinTile> {coin} </CoinTile>)}
+            {Object.keys(this.state.coinList).slice(0, 100).map(coin =>
+                <CoinTile>
+                    <CoinHeader>
+                        <div>{this.state.coinList[coin].CoinName}</div>
+                        <CoinSymbol>{this.state.coinList[coin].Symbol}</CoinSymbol>
+                    </CoinHeader>
+                    <img style={{height: '50px'}} src={`http://cryptocompare.com/${this.state.coinList[coin].ImageUrl}`} />
+                </CoinTile>
+            )}
+
         </CoinGrid>
     )
 }
---
+++ src/Style.js
@@ -0,0 +1,17 @@
+export const color = '#rgba(0, 0, 0, .3)';
+export const color2 = 'green';
+
+
+
+export const backgroundColor = `background-color: ${color}`;
+export const greenBackgroundColor = `background-color: ${color2}`;
+
+
+export const fontColorBlue =  `color: blue`;
+export const fontColorWhite = `color: #FFF`;
+export const subtleBoxShadow = `box-shadow: 0px 0px 5px 1px #000`;
+export const greenBoxShadow = `box-shadow: 0px 0px 5px 1px #green`;
+export const redBoxShadow = `box-shadow: 0px 0px 5px 1px #pink`;
+
+
+
